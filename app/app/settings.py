61da2e64a936fe0b5a93855f81743f20c4a97e18
RickVanGogh <glogau@hornetsecurity.com>
2023-08-07 12:59:22 +0200

    Update

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': "127.0.0.1",
+        'PORT': "3306",
+        "USER": "docker",
+        "PASSWORD": "docker",
+        "NAME": "docker"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
