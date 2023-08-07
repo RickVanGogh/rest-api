a243a31dff543323060e9fe2faaa00cebe59bbfe
RickVanGogh <glogau@hornetsecurity.com>
2023-08-07 14:00:41 +0200

    Update

--- app/app/test.py
+++ app/app/test.py
@@ -1,9 +1,10 @@
 """
 Sample tests
 """
+
 from django.test import SimpleTestCase
 
-from app.app import calc
+from app import calc
 
 
 class CalcTests(SimpleTestCase):
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available.
 """
 import time
 
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
